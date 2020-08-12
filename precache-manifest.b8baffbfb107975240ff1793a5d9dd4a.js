fa2d53f327dded583450689993136401a5897c50
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-08-12 01:08:58 +0000

    Deploy UlisesGomezDW/SocialRedFrontend to github.com/UlisesGomezDW/SocialRedFrontend.git:gh-pages

---
+++ precache-manifest.b8baffbfb107975240ff1793a5d9dd4a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cc44834fe1b9b7592898ced2ac9fab5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4e64ba387731f184db3a",
+    "url": "/static/css/2.9c8c8187.chunk.css"
+  },
+  {
+    "revision": "000b2ec33c88171939b3",
+    "url": "/static/css/main.bf138e30.chunk.css"
+  },
+  {
+    "revision": "4e64ba387731f184db3a",
+    "url": "/static/js/2.e643f4e8.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.e643f4e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "000b2ec33c88171939b3",
+    "url": "/static/js/main.f16066a0.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
